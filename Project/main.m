2fb121f04bf6b182b5e72b23afc7c2ea81ef64fc
jjv432 <jackvranicar43@gmail.com>
2024-11-13 14:13:53 -0500

    Animation with increasing speed; small angle approximation still

--- Project/main.m
+++ Project/main.m
@@ -16,9 +16,22 @@ g = 9.81;
 
 %% Transfer Function
 
-T = tf([1], [I, 0, m*g]);
+T = tf([1], [I, 0, m*g*(l/2)]);
 
 resp = step(T, 10);
+
+%% State Spaces
+
+A = [0 1; (m*g*l/(2*I)), 0];
+B = [0; -1/I];
+C = [1 0];
+D = [0];
+
+G = ss(A, B, C, D);
+
+time = linspace(0, 5, 1000);
+tau = 0 * ones(size(time, 2), 1);
+ss_resp = lsim(G, tau, time, [pi; 0]);
 
 %% Animate Rectangle
 
@@ -28,9 +41,10 @@ yvals = l*[0 1 1 0];
 coords = [xvals; yvals];
 figure();
 thetas = linspace(0, 2*pi, 50);
-for i = 1:length(thetas)
 
-    theta = thetas(i);
+for i = 1:length(ss_resp)
+
+    theta = -pi/2 - ss_resp(i);
     rotMatrix = [sin(theta), cos(theta); -cos(theta), sin(theta)];
 
     rotCoords = rotMatrix* coords;
